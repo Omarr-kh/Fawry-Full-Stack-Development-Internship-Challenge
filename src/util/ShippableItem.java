3ba3c82898e1289d17d33e467b795e425524e5cb
Omar <omar1752003@gmail.com>
2025-07-04 22:43:17 +0300

    Add ShippableItem interface and ShippingService class for handling shipment of items.

---
+++ src/util/ShippableItem.java
@@ -0,0 +1,6 @@
+package util;
+
+public interface ShippableItem {
+    String getName();
+    double getWeight();
+} 
---
+++ src/util/ShippingService.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.util.List;
+
+public class ShippingService {
+    public static void ship(List<ShippableItem> items) {
+        double totalWeight = 0;
+        System.out.println("** Shipment notice **");
+        for (ShippableItem item : items) {
+            System.out.printf("%s %.0fg\n", item.getName(), item.getWeight() * 1000);
+            totalWeight += item.getWeight();
+        }
+        System.out.printf("Total package weight %.1fkg\n", totalWeight);
+    }
+} 
